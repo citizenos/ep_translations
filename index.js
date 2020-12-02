71fec84a06bfb1a7d9cac0b63c226db48451d19b
ilmar <ilmar.tyrk@gmail.com>
2020-12-02 10:36:13 +0200

    update code

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
-'use strict'
+'use strict';
 
 const cloneOrPull = require('git-clone-or-pull');
 const path = require('path');
 const fs = require('fs');
 
-let languages = {};
+const languages = {};
 let settings = {};
-let savePath = path.join(process.cwd(),'node_modules', 'ep_translations', 'locales'); //defaults to node_module folder
+// defaults to node_module folder
+let savePath = path.join(process.cwd(), 'node_modules', 'ep_translations', 'locales');
 
-const loadTranslations = async function () {
-  return new Promise (function (resolve, reject) {
-    fs.readdir(savePath, function (err, files) {
-      if (err) return reject(err);
-      if (!files || !files.length) {
-        return resolve();
+const loadTranslations = async () => {
+  fs.readdir(savePath, (err, files) => {
+    if (err) throw new Error(err);
+    if (!files || !files.length) {
+      return;
+    }
+    let i = 0;
+    let called = false;
+    const filtered = files.filter((file) => {
+      if (file.indexOf('.json') > -1 && file !== 'source.json') {
+        return file;
       }
-      let i = 0;
-      let called = false;
-      const filtered = files.filter(function (file) {
-        if (file.indexOf('.json') > -1 && file !== 'source.json') {
-          return file;
-        }
-      });
+    });
 
-      filtered.forEach(function (file) {
-        const lang = file.split('.')[0];
-        fs.readFile(path.join(savePath, file), function (err, data) {
-          try {
-            const languageData = JSON.parse(data);
-            Object.keys(languageData).forEach(function (key) {
-                if (languageData[key] == null || languageData[key] === '') delete languageData[key];
-            });
-            languages[lang] = languageData;
-            i++;
-            if(i === files.length && !called) {
-                called = true;
-                return resolve();
-            }
-          } catch (err) {
-            console.error('ep_translations', file, err);
-            return reject(err);
+    filtered.forEach((file) => {
+      const lang = file.split('.')[0];
+      fs.readFile(path.join(savePath, file), (err, data) => {
+        try {
+          const languageData = JSON.parse(data);
+          Object.keys(languageData).forEach((key) => {
+            if (languageData[key] == null || languageData[key] === '') delete languageData[key];
+          });
+          languages[lang] = languageData;
+          i++;
+          if (i === files.length && !called) {
+            called = true;
+            return true;
           }
-        });
+        } catch (err) {
+          console.error('ep_translations', file, err);
+          throw new Error(err);
+        }
       });
     });
   });
 };
 
-exports.loadSettings = async function (hook_name, context) {
+exports.loadSettings = async (hook, context) => {
   settings = context.settings.ep_translations;
   if (settings.savePath) {
     savePath = settings.savePath;
   }
   if (settings.type === 'git') {
-    let options = {
-      path: savePath
+    const options = {
+      path: savePath,
     };
     if (settings.branch) {
       options.branch = settings.branch;
@@ -63,7 +62,7 @@ exports.loadSettings = async function (hook_name, context) {
       options.implementation = settings.implementation;
     }
 
-    await cloneOrPull(settings.path, options, async function(err, res) {
+    await cloneOrPull(settings.path, options, async (err, res) => {
       if (err) {
         console.error('ep_translations', err, res);
       }
@@ -75,6 +74,6 @@ exports.loadSettings = async function (hook_name, context) {
   }
 };
 
-exports.clientVars = async function (hook, context) {
-  return {ep_translations: {languages}}
-}
+exports.clientVars = (hook, context, cb) => {
+  return cb({ep_translations: { languages }});
+};
--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,22 @@
-'use strict'
+'use strict';
 
-exports.aceInitialized = function (hook, name) {
-  var translations = window.html10n.translations;
+exports.aceInitialized = (hook, name) => {
+  const translations = window.html10n.translations;
   window.html10n.translations = translations;
 
-  var languages = window.clientVars.ep_translations.languages;
-  var lc = Object.keys(languages);
+  const languages = window.clientVars.ep_translations.languages;
+  const lc = Object.keys(languages);
 
-  $.each(lc, function (key, lang) {
+  $.each(lc, (key, lang) => {
     $.extend(window.html10n.loader.langs[lang], languages[lang]);
   });
 
-  window.html10n.build(lc, function (err, done) {
+  window.html10n.build(lc, (err, done) => {
     if (err) {
       throw err;
     }
 
-    var language = document.cookie.match(/language=((\w{2,3})(-\w+)?)/);
+    let language = document.cookie.match(/language=((\w{2,3})(-\w+)?)/);
     if (language) language = language[1];
 
     html10n.localize([language, navigator.language, navigator.userLanguage, 'en']);
@@ -25,4 +25,3 @@ exports.aceInitialized = function (hook, name) {
     }
   });
 };
-
