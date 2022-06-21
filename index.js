7eed9ce0e6056ad8b693068304db5b3c27758294
ilmar <ilmar.tyrk@gmail.com>
2022-06-21 15:52:22 +0300

    Update packages and cloning

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const cloneOrPull = require('git-clone-or-pull');
+const NodeGit = require('nodegit');
 const path = require('path');
 const fs = require('fs');
 
@@ -9,72 +9,86 @@ let settings = {};
 // defaults to node_module folder
 let savePath = path.join(process.cwd(), 'node_modules', 'ep_translations', 'locales');
 
-const loadTranslations = async () => {
-  fs.readdir(savePath, (err, files) => {
-    if (err) throw new Error(err);
-    if (!files || !files.length) {
-      return;
+const cloneOrPull = async (url, options) => {
+  if (!options.branch) {
+    options.branch = 'master';
+  }
+  try {
+    fs.accessSync(options.path);
+    // Cloned already; we need to pull
+    const repository = await NodeGit.Repository.open(options.path);
+    await repository.fetchAll();
+    return repository.mergeBranches(options.branch, `origin/${options.branch}`);
+  } catch (err) {
+    // Not yet cloned
+    console.log('ERR', err);
+    try {
+      fs.mkdirSync(savePath, {recursive: true});
+      return NodeGit.Clone(url, options.path); // eslint-disable-line new-cap
+    } catch (err) {
+      console.log('CLONE ERROR', err);
     }
-    let i = 0;
-    let called = false;
-    const filtered = files.filter((file) => {
-      if (file.indexOf('.json') > -1 && file !== 'source.json') {
-        return file;
-      }
-    });
+  }
+};
 
-    filtered.forEach((file) => {
-      const lang = file.split('.')[0];
-      fs.readFile(path.join(savePath, file), (err, data) => {
-        try {
-          const languageData = JSON.parse(data);
-          Object.keys(languageData).forEach((key) => {
-            if (languageData[key] == null || languageData[key] === '') delete languageData[key];
-          });
-          languages[lang] = languageData;
-          i++;
-          if (i === files.length && !called) {
-            called = true;
-            return true;
-          }
-        } catch (err) {
-          console.error('ep_translations', file, err);
-          throw new Error(err);
-        }
+const loadTranslations = async () => {
+  const files = fs.readdirSync(savePath);
+  if (!files || !files.length) {
+    return;
+  }
+  const filtered = files.filter((file) => {
+    if (file.indexOf('.json') > -1 && file !== 'source.json') {
+      return file;
+    }
+  });
+  let i = 0;
+  let called = false;
+  filtered.forEach(async (file) => {
+    const lang = file.split('.')[0];
+    const data = await fs.readFileSync(path.join(savePath, file));
+    try {
+      const languageData = JSON.parse(data);
+      Object.keys(languageData).forEach((key) => {
+        if (languageData[key] == null || languageData[key] === '') delete languageData[key];
       });
-    });
+      languages[lang] = languageData;
+      i++;
+      if (i === files.length && !called) {
+        called = true;
+        return true;
+      }
+    } catch (err) {
+      console.error('ep_translations', file, err);
+      throw new Error(err);
+    }
   });
 };
 
 exports.loadSettings = async (hook, context) => {
-  settings = context.settings.ep_translations;
-  if (settings.savePath) {
-    savePath = settings.savePath;
-  }
-  if (settings.type === 'git') {
-    const options = {
-      path: savePath,
-    };
-    if (settings.branch) {
-      options.branch = settings.branch;
+  try {
+    settings = context.settings.ep_translations;
+    if (settings.savePath) {
+      savePath = settings.savePath;
     }
-    if (settings.implementation) {
-      options.implementation = settings.implementation;
-    }
-
-    if (!fs.existsSync(savePath)) {
-      fs.mkdirSync(savePath, {recursive: true});
-    }
-
-    await cloneOrPull(settings.path, options, async (err, res) => {
-      if (err) {
-        console.error('ep_translations', err, res);
+    if (settings.type === 'git') {
+      const options = {
+        path: savePath,
+      };
+      if (settings.branch) {
+        options.branch = settings.branch;
+      }
+      if (settings.implementation) {
+        options.implementation = settings.implementation;
       }
 
+      await cloneOrPull(settings.path, options);
+
       return await loadTranslations();
-    });
-  } else {
-    return await loadTranslations();
+    } else {
+      return await loadTranslations();
+    }
+  } catch (err) {
+    console.log('ep_translations ERROR', err);
   }
 };
 
